a60517b5bbd5f2f69935e14614766bf949da68c4
Azenwrath <dana.stubkjaer@gmail.com>
2017-12-11 11:38:34 -0800

    Adds image preload and favicon

--- sign/static/sign/js/fingerspelling.js
+++ sign/static/sign/js/fingerspelling.js
@@ -10,7 +10,7 @@ let currentWord = wordlist.sample(); //TODO: Make this an object
 let currentSpeed = 1000;
 
 let changeLetter = function (letter) {
-    var elem = document.getElementById("sign-window");
+    let elem = document.getElementById("sign-window");
     // elem.style.background = url('/static/sign/img/b.gif');
     elem.style.backgroundImage = 'url(/static/sign/img/' + letter.toLowerCase() + '.gif)';
     console.log('Changing sign');
@@ -56,25 +56,30 @@ document.getElementById('slower').addEventListener("click", function () {
     currentSpeed += 100;
 });
 
-// Example precache function from Stack Overflow for later. TODO: Implement
-// function preloadImages(array) {
-//     if (!preloadImages.list) {
-//         preloadImages.list = [];
-//     }
-//     var list = preloadImages.list;
-//     for (var i = 0; i < array.length; i++) {
-//         var img = new Image();
-//         img.onload = function() {
-//             var index = list.indexOf(this);
-//             if (index !== -1) {
-//                 // remove image from the array once it's loaded
-//                 // for memory consumption reasons
-//                 list.splice(index, 1);
-//             }
-//         }
-//         list.push(img);
-//         img.src = array[i];
-//     }
-// }
-//
-// preloadImages(["url1.jpg", "url2.jpg", "url3.jpg"]);
+
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    let list = preloadImages.list;
+    for (let i = 0; i < array.length; i++) {
+        let img = new Image();
+        img.onload = function() {
+            let index = list.indexOf(this);
+            // if (index !== -1) {
+            list.splice(index, 1);
+            // }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+let imageList = [];
+for (i in alphabet) {
+    const letterUrl = '/static/sign/img/' + alphabet[i] + ".gif";
+    imageList.push(letterUrl);
+};
+
+preloadImages(imageList);
